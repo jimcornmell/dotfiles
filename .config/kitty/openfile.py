62f8124dbeb6817736d08c1ba44a960fdfdd9a7e
Jim Cornmell <jim.cornmell@costcutter.com>
2021-05-11 17:36:17 +0100

    Disable java for now

--- .config/kitty/openfile.py
+++ .config/kitty/openfile.py
@@ -3,6 +3,6 @@ import os
 
 def main(args):
     if os.path.exists(args[1]):
-        os.system('$HOME/bin/openf "' + args[1] + '"')
+        os.system('$HOME/bin/dotfiles/bin/openf "' + args[1] + '"')
     else:
-        os.system('$HOME/bin/openf "' + os.getcwd() + '/' + args[1] + '"')
+        os.system('$HOME/bin/dotfiles/bin/openf "' + os.getcwd() + '/' + args[1] + '"')
